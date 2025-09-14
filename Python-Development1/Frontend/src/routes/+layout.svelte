b935b43d5f7e67f97e0f889a21136694cc5b503f
sanda-h <24110001@oic-ok.ac.jp>
2025-09-15 04:02:34 +0900

    整備

--- Python-Development1/Frontend/src/routes/+layout.svelte
+++ Python-Development1/Frontend/src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-
+	//import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/Header.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href="/favicon.png" /> <!--タブの画像-->
 </svelte:head>
+<Header /> <!--自分で自己閉じしている。Headerコンポーネントを指定して表示している。-->
+<main>
+	{@render children()} <!--親(+page)コンポーネントの渡した内容を表示する-->
+</main>
 
-{@render children?.()}
+
+
